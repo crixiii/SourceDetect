6c287cba78147e8f37ad21d6ec3c239e954e3011
andrewmoore73 <95614601+andrewmoore73@users.noreply.github.com>
2024-06-26 11:20:34 +1200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+NAME = 'SourceDetect'
+DESCRIPTION = 'Build, train, and implement a CNN model for detecting real objects in TESS data'
+URL = 'https://github.com/andrewmoore73/SourceDetect'
+EMAIL = 'andrewnmoore73@gmail.com'
+AUTHOR ='Andrew Moore'
+VERSION = '1.0.0'
+REQUIRED = ['keras>=3.0',
+            'tensorflow>=2.16',
+            ]
+
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    url=URL,
+    author_email=EMAIL,
+    author=AUTHOR,
+    packages=['sourcedetect'],
+    install_requires=REQUIRED
+)
